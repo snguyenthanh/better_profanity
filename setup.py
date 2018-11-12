966fb3564c617e67792c1dd056b9975da558c1ba
unknown <thanhson16198@gmail.com>
2018-11-12 16:47:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 import setuptools
+from better_profanity import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="better_profanity",
-    version="0.1",
+    version=__version__,
     author="Son Nguyen Thanh",
     author_email="thanhson16198@gmail.com",
-    description="A Python library to clean swear words in strings",
+    description="A Python library to clean English swear words in strings",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/snguyenthanh/better_profanity",
     classifiers=[
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires=">3.6",
     packages=setuptools.find_packages(),
     data_files=[("wordlist", ["better_profanity/profanity_wordlist.txt"])],
     package_data={"better_profanity": ['profanity_wordlist.txt']},
