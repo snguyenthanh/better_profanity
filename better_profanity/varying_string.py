a2df3967d94dbb02f8dc0fd2d1b88abb2948fc98
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-10-03 02:59:37 -0700

    Use character-by-character string comparison
    Speeds up profanity detection by roughly 10x.

--- better_profanity/varying_string.py
+++ better_profanity/varying_string.py
@@ -22,48 +22,25 @@ class VaryingString:
             variant_thres (int): Maximum number of variants to store in a batch.
         """
         self._original = string
-        self._sliced_variants = []
-        slice_combos = []
-        num_slice_variants = 1
-        for i, char in enumerate(iter(string)):
-            if char in char_map:
-                chars = char_map[char]
-                num_slice_variants *= len(chars)
-                if num_slice_variants > variant_thres:
-                    # Finalize slice.
-                    variants = VaryingString._create_variants(slice_combos)
-                    self._sliced_variants.append(variants)
 
-                    # Move to a new slice.
-                    slice_combos = [chars]
-                    num_slice_variants = 1
-                else:
-                    slice_combos.append(chars)
-            else:
-                slice_combos.append((char,))
-        # Finalize final slice.
-        variants = VaryingString._create_variants(slice_combos)
-        self._sliced_variants.append(variants)
         # There is not necessarily a single length for all of this string's variants.
         # Some character substitutions may include more than one character or empty
         # substitutions.
         self._min_len = 0
         self._max_len = 0
-        for slices in self._sliced_variants:
-            slice_min = None
-            slice_max = None
-            for s in slices:
-                ls = len(s)
-                if slice_min is None:
-                    slice_min = ls
-                    slice_max = ls
-                else:
-                    if ls < slice_min:
-                        slice_min = ls
-                    if ls > slice_max:
-                        slice_max = ls
-            self._min_len += slice_min
-            self._max_len += slice_max
+
+        # Create list of all possible character combinations.
+        self._char_combos = []
+        for char in self._original:
+            if char in char_map:
+                self._char_combos.append(char_map[char])
+                lens = [len(c) for c in char_map[char]]
+                self._min_len += min(lens)
+                self._max_len += max(lens)
+            else:
+                self._char_combos.append((char,))
+                self._min_len += 1
+                self._max_len += 1
 
     def __str__(self):
         return self._original
@@ -78,20 +55,24 @@ class VaryingString:
             len_other = len(other)
             if len_other < self._min_len or len_other > self._max_len:
                 return False
-            other_slice = other
-            for var_slice in self._sliced_variants:
-                match_found = False
-                for variant in var_slice:
-                    len_var = len(variant)
-                    if len(other_slice) >= len_var:
-                        if variant == other_slice[:len_var]:
-                            other_slice = other_slice[len_var:]
-                            match_found = True
-                if not match_found:
+            # We use a list of slices instead of a single slices to account for
+            # character substitutions that contain multiple characters.
+            slices = [other]
+            for chars in self._char_combos:
+                new_slices = []
+                for char in chars:
+                    if not char:
+                        new_slices.extend(slices)
+                    len_char = len(char)
+                    for sl in slices:
+                        if sl[:len_char] == char:
+                            new_slices.append(sl[len_char:])
+                if len(new_slices) == 0:
                     return False
-            if other_slice:
-                return False
-            else:
-                return True
+                slices = new_slices
+            for sl in slices:
+                if len(sl) == 0:
+                    return True
+            return False
         else:
             return False
